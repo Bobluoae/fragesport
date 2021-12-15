a6f6ddeba7bdf401bee9a89adcf803f0e516948f
Erik Källgren <erik.kallgren@learnet.se>
2021-12-15 15:39:59 +0100

    lite uppdateringar

--- index.php
+++ index.php
@@ -3,16 +3,4 @@ include "visual/header.php";
 include "visual/navbar.php";
 include "visual/main.php";
 include "visual/footer.php";
-//include "visual/pages/fragor.php";
-
-$questions = file_get_contents("questions.json");
-
-$fragorArr = json_decode($questions);
-
-foreach ($fragorArr as $fragaObj) {
-	echo "<hr>" . $fragaObj->questionStr;
-
-	foreach ($fragaObj->choicesArr as $svarStr) {
-		echo "<br>" . $svarStr;
-	}
-}
+include "visual/pages/fragor.php";
--- visual/pages/fragor.php
+++ visual/pages/fragor.php
@@ -1,5 +1,14 @@
 <?php 
 
-$questions = json_decode("questions.json");
 
-var_dump($questions);
+$questions = file_get_contents("questions.json");
+
+$fragorArr = json_decode($questions);
+
+foreach ($fragorArr as $fragaObj) {
+	echo "<hr>" . $fragaObj->questionStr;
+
+	foreach ($fragaObj->choicesArr as $svarStr) {
+		echo "<br>" . $svarStr;
+	}
+}
