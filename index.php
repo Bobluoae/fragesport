4afd016229b68c6e8e2cabee159b7b27ed9ad5bc
Erik Källgren <erik.kallgren@learnet.se>
2022-02-22 13:20:17 +0100

    uppdateringggggg

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php 
 include "fragor/fragesport.php";
+include "visual/header.php";
 session_start();
 
 if (!isset($_GET["quiz"])) {
@@ -14,17 +15,16 @@ if (!isset($_SESSION["quiz"]) || $_GET["quiz"] == "notstart") {
 $quiz = new Quiz("questions.json");
 $_SESSION["quizclass"] = $quiz;
 
+
+
 if (isset($_POST["form"])) {
 	
 	if ($_POST["form"] == "nameform") {
 
-
-
-
-
 		if ($_POST["name"] == "") {
 		
-			echo "<a href='/''>Skriv ditt namn för fan</a>";
+			echo "<strong style = 'color: red';> Du måste skriva ett namn!</strong>";
+
 		} else if (is_string($_POST["name"])){ 
 			//Spara i session
 			$_SESSION["namn"] = $_POST["name"];
@@ -34,14 +34,11 @@ if (isset($_POST["form"])) {
 	} 
 	if ($_POST["form"] == "questionform") {
 		//En svara har besvarats
-
+		//TODO
 	}	
 }
 
-echo "<br>";
-var_dump($_POST); echo "<br>";
 
-include "visual/header.php";
 
 if (!isset($_POST["quiz"])) {
 	$_POST["quiz"] = "";
--- visual/questionform.php
+++ visual/questionform.php
@@ -3,23 +3,35 @@ $q = $_SESSION["quizclass"];
 $a = $q->getAnswers($_GET["pagenum"]);
 
 
- ?>
-	<form action="?pagenum=<?php echo $_GET["pagenum"]+1?>" method="POST">
-		<?php 
+?>
+<form action="?pagenum=<?php echo $_GET["pagenum"]+1?>" method="POST">
+	<?php 
 
-		for ($i=0; $i < count($a); $i++) { 
-			
-			echo "<input type=\"radio\" name=\"answer\" value=\"$a[$i]\"> $a[$i] <br>";
-
-		}
-
-		 ?>
+	for ($i=0; $i < count($a); $i++) { 
 		
-<!-- 		<input type="radio" name="answer" value="2"> Svar 2 <br>
-		<input type="radio" name="answer" value="3"> Svar 3 <br>
-		<input type="radio" name="answer" value="4"> Svar 4 <br> -->
-		<input type="submit" value="next">
-		<input type="submit" value="back"> <br>
-		<input type="hidden" name="form" value="questionform">
-		
-	</form>
+		echo "<input type=\"radio\" name=\"answer\" value=\"$a[$i]\"> $a[$i] <br>";
+
+	}?>
+
+	<input type="submit" value="Svara">
+	<br>
+	<input type="hidden" name="form" value="questionform">
+	
+</form>
+
+<p id="output"></p>
+
+<script>
+        // add an event listener for the change event
+    const radioButtons = document.querySelectorAll('input[name="answer"]');
+    for(const radioButton of radioButtons){
+        radioButton.addEventListener('change', showSelected);
+    }        
+    
+    function showSelected(e) {
+        console.log(e);
+        if (this.checked) {
+            document.querySelector('#output').innerText = `You selected ${this.value}`;
+        }
+    }
+</script>
